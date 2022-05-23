a573a9914a81677748c21856623e8dd2b73dfba1
Ivan Alvarado <ivan.alvarado1090@gmail.com>
2022-05-23 13:50:31 -0500

    Gifs App / servicios / httprequest / localstorage

---
+++ src/app/gifs/resultado/resultado.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-resultado',
+  templateUrl: './resultado.component.html',
+  styleUrls: ['./resultado.component.css']
+})
+export class ResultadoComponent {
+
+  get resultados() {
+    return this.gifsService.resultados;
+  }
+  constructor(private gifsService: GifsService) {
+
+  }
+
+}
